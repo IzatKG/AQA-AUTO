5540270ba107e06d8382cc665d18c70827a2994c
Izat <kerimbaevazat@gmail.com>
2025-01-16 19:53:05 +0600

    some changes

--- src/main/java/UI/Main.java
+++ src/main/java/UI/Main.java
@@ -2,6 +2,8 @@ package UI;
 
 public class Main {
     public static void main(String[] args) {
-
+        //создать ветку   git branch
+        //создать ветку  и переключение на ветку  git checkout -b
+        // переключение git checkout
     }
 }
