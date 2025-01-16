7e3af43a38f4b1a240962186656e5ffdfbb8a7a1
Tastan <tashtan.tk@gmail.com>
2025-01-16 21:27:21 +0600

    My commit by Tashtan

---
+++ src/main/java/UI/TashCass.java
@@ -0,0 +1,9 @@
+package UI;
+
+public class TashCass {
+    public static void main(String[] args) {
+        System.out.println("Hello Students");
+        System.out.println("Changing my text");
+        System.out.println("MMM");
+    }
+}
