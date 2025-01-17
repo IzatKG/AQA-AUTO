0f9b90428299bb89f412972189a4200d52581713
Erm <bekmuratov-92.92@mail.ru>
2025-01-17 20:56:45 +0600

    changes

--- src/main/java/UI/Ermek/Main.java
+++ src/main/java/UI/Ermek/Main.java
@@ -5,11 +5,7 @@ public class  Main {
         //создать ветку   git branch
         //создать ветку  и переключение на ветку  git checkout -b
         // переключение git checkout
-<<<<<<< HEAD:src/main/java/UI/Ermek/Main.java
-        System.out.println("Hello!!!");
-=======
         // git merge main  cлияние изменений
         // git status - показывает состояние ветки
->>>>>>> 07ded85338e57a5078e3a06ed3d18ae97c84689c:src/main/java/UI/Main.java
     }
 }
