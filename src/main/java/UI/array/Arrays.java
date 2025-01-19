03ae30ad47d930462477e1915b004797f87b32c8
Joldosh <joldosh5577@gmail.com>
2025-01-19 21:05:03 +0600

    Arrays

---
+++ src/main/java/UI/array/Arrays.java
@@ -0,0 +1,25 @@
+package UI.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Arrays {
+    public static int[] makeLast(int[] nums) {
+        int[] num = new int[2 * nums.length];
+        num[num.length - 1] = nums[nums.length - 1];
+        return num;
+    }
+
+    @Test
+    @DisplayName("Make Last")
+    public void makeLast() {
+
+   int [] numm = {4,5,6};
+   int [] result = makeLast(numm);
+        System.out.println("Result: ");
+        for (int n : result){
+            System.out.println(n + " ");
+        }
+    }
+
+}
