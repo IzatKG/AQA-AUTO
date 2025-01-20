8968360372d119ad58519f847850f60da7d78662
Izat <kerimbaevazat@gmail.com>
2025-01-20 20:22:53 +0600

    added sout

--- src/main/java/UI/ConsentPage.java
+++ src/main/java/UI/ConsentPage.java
@@ -4,5 +4,6 @@ public class ConsentPage {
     public static void main(String[] args) {
         System.out.println("Consent");
         System.out.println("JAVA");
+        System.out.println("PHYTON");
     }
 }
