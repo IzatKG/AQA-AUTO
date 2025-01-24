40c436133a6abd8b5d638accd56a0abd1e0bf6cf
Izat <kerimbaevazat@gmail.com>
2025-01-24 21:15:16 +0600

    Добавлены новые классы

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,16 +1,56 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
-public class MyTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-    public static void main(String[] args) {
+import java.util.Properties;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Tag("smoke")
+public class MyTest{
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("BEFORE_EACH");
+    }
+    @AfterEach
+    public void afterEach(){
+        System.out.println("AFTER_EACH");
+    }
+    @BeforeAll
+    public static void setUp() {
+        System.out.println("Berfore_ALL");
 
     }
 
-   @Test
-   @DisplayName("check the foodButton")
-    public void myFirstTest(){
-       System.out.println("My Test ");
-   }
+    @AfterAll
+    public static void tearDown() {
+        // driver.quit();
+        System.out.println("After_ALL");
+    }
+
+    @Test
+    @DisplayName("check the foodButton")
+    @Order(0)
+    public void myFirstTest() {
+        //  driver.get("  https://demoqa.com/text-box");
+        System.out.println("1 test");
+    }
+
+    @Test
+    @DisplayName("check the foodButton")
+    @Order(1)
+
+    public void mySecondTest() {
+        System.out.println("2 test");
+
+    }
+    @Test
+    @DisplayName("check the foodButton")
+    @Order(2)
+    @Disabled("Этот тест временно отключен")
+    public void myThirdTest() {
+        System.out.println("3 test");
+
+    }
 }
