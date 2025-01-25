eda0d5ee7bf3852ca2bf3ffe4bf1d6fa08f3d446
Joldosh <joldosh5577@gmail.com>
2025-01-25 10:04:43 +0600

    new Class 12

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-=======
->>>>>>> refs/remotes/origin/main
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
