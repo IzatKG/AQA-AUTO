655c37d3a96836e00932eda5cc1f3235126858a2
Izat <kerimbaevazat@gmail.com>
2025-01-29 20:34:52 +0600

    added a new RadioButtonTest class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,6 +3,8 @@ import UI.driver.Driver;
 import UI.helper.WebElementActions;
 import UI.owner.ConfigProperties;
 import UI.pages.CheckboxPage;
+import UI.pages.MainPage;
+import UI.pages.RadioButtonPage;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +12,8 @@ import org.openqa.selenium.WebDriver;
 
 public class BaseTest {
    public static CheckboxPage checkboxPage;
+   public static RadioButtonPage radioButtonPage;
+   public static MainPage mainPage;
    public static WebElementActions webElementActions;
     public static WebDriver driver;
    static ConfigProperties config = ConfigFactory.create(ConfigProperties.class);
@@ -17,6 +21,8 @@ public class BaseTest {
     @BeforeAll
     public static void setUp() {
        checkboxPage =new CheckboxPage();
+       radioButtonPage = new RadioButtonPage();
+       mainPage = new MainPage();
        webElementActions = new WebElementActions();
         // PropertyReader.readProperty();
          driver = Driver.getDriver();
