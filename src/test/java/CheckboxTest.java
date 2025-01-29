80ddf462720affc91e53bb189dbb7d215b0a0a20
Meder <abdymusaevvv@gmail.com>
2025-01-29 20:41:57 +0600

    29.01.2025

--- src/test/java/CheckboxTest.java
+++ src/test/java/CheckboxTest.java
@@ -20,19 +20,7 @@ public class CheckboxTest extends BaseTest {
     @Test
     public void checkCheckboxTest() {
         
-      //  WebElement elementsButton = driver.findElement(By.xpath("//h5[text()='Elements']"));
-       // scrollToElement(driver, elementsButton);
-        //elementsButton.click();
-        //  WebElement checkboxBtn = driver.findElement(By.xpath("//span[text()='Check Box']"));
-        // checkboxBtn.click();
-        //  assertTrue(checkboxBtn.isDisplayed());
-        // assertTrue(checkboxBtn.isEnabled());
 
-
-        // driver.findElement(By.xpath("(//button[@type='button'])[4]")).click();
-        // WebElement documentChekcbox = driver.findElement(By.xpath("(//span[@class='rct-checkbox'])[2]"));
-
-        // scrollToElement(driver,documentChekcbox);
     }
 
     public static void scrollToElement(WebDriver driver, WebElement element) {
