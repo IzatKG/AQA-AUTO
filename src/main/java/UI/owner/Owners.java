0480ea0d40c68a88bd7bfa16be3353fbcbe22dbd
Meder <abdymusaevvv@gmail.com>
2025-02-13 19:03:52 +0600

    Delete src directory
    каша заново начал

--- src/main/java/UI/owner/Owners.java
+++
@@ -1,6 +0,0 @@
-package UI.owner;
-
-public class Owners {
-    public static final String IZAT_YKLASOV = "izat-ms";
-    public static final String MEDER_ABDYMUSAEV="meders";
-}
--- src/main/java/UI/pages/MainPage.java
+++
@@ -1,9 +0,0 @@
-package UI.pages;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class MainPage extends BasePage {
-    @FindBy(xpath = "//span[text()='Radio Button']")
-    public WebElement radioButton;
-}
